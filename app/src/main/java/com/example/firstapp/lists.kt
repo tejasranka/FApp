ed665e55ba9dd2a39948ea8c59c4195e3397a1d5
tejasranka <rankatm17.instru@coep.ac.in>
2021-07-13 11:45:25 +0530

    mutablelists

--- app/src/main/java/com/example/firstapp/lists.kt
+++ app/src/main/java/com/example/firstapp/lists.kt
@@ -1,30 +1,30 @@
 package com.example.firstapp
 
 class lists {
-fun main() {
-    val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
-    println("List: $numbers")
-    println("Size: ${numbers.size}")
-    println("First element is: ${numbers[0]}")
-    println("Second element is: ${numbers[1]}")
-    println("last  element is: ${numbers[numbers.size - 1]}")
-    println("Last index is: ${numbers.size - 1}")
+    fun main() {
+        val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+        println("List: $numbers")
+        println("Size: ${numbers.size}")
+        println("First element is: ${numbers[0]}")
+        println("Second element is: ${numbers[1]}")
+        println("last  element is: ${numbers[numbers.size - 1]}")
+        println("Last index is: ${numbers.size - 1}")
 
-    val moreItems = listOf("pasta","momos","vada pav")
-    val entries = mutableListOf<String>()//why giving parameter string necessary
-    println("add noodles: ${entries.add("noodles")}")
-    println("add spaghetti: ${entries.add("spaghetti")}")
-    println("adding a list: ${entries.addAll(moreItems)}")
-    //entries.add(10)
-    println("Remove spaghetti: ${entries.remove("spaghetti")}")
-    println("Remove rice: ${entries.remove("rice")}")
-    println("Remove rice: ${entries.removeAt(0)}")
-    println("Remove all/ clear list: ${entries.clear()}")
+        val moreItems = listOf("pasta","momos","vada pav")
+        val entries = mutableListOf<String>()//why giving parameter string necessary
+        println("add noodles: ${entries.add("noodles")}")
+        println("add spaghetti: ${entries.add("spaghetti")}")
+        println("adding a list: ${entries.addAll(moreItems)}")
+        //entries.add(10)
+        println("Remove spaghetti: ${entries.remove("spaghetti")}")
+        println("Remove rice: ${entries.remove("rice")}")
+        println("Remove rice: ${entries.removeAt(0)}")
+        println("Remove all/ clear list: ${entries.clear()}")
 
 
 
-    println("Entries: $entries")
-    println("empty? : ${entries.isEmpty()}")
-}
+        println("Entries: $entries")
+        println("empty? : ${entries.isEmpty()}")
+    }
 
 }
